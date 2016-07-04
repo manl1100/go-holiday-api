8e84492e1299d804d90130552065d94781f1da1d
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-04 17:07:56 -0500

    Route struct and router file

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ package main
 import (
   "log"
   "net/http"
-
-  "github.com/gorilla/mux"
 )
 
 func main() {
-
-  router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/", Index)
+  router := NewRouter()
   log.Fatal(http.ListenAndServe(":8080", router))
 }
