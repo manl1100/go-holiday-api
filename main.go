35eb900b89d717b6852f98e3bf03c84a4a0779a2
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-04 16:48:46 -0500

    Separated into handler and router file

--- main.go
+++ main.go
@@ -2,8 +2,6 @@
 package main
 
 import (
-  "fmt"
-  "html"
   "log"
   "net/http"
 
@@ -15,8 +13,4 @@ func main() {
   router := mux.NewRouter().StrictSlash(true)
   router.HandleFunc("/", Index)
   log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
