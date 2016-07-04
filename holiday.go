8fad4bae1372e379d0611599ee953ee7037b5608
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-04 01:16:34 -0500

    Model and additional route

--- holiday.go
+++ holiday.go
@@ -1,7 +1,7 @@
-
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "html"
   "log"
@@ -11,12 +11,27 @@ import (
 )
 
 func main() {
-
   router := mux.NewRouter().StrictSlash(true)
   router.HandleFunc("/", Index)
+  router.HandleFunc("/todos", TodoIndex)
+  router.HandleFunc("/todos/{todoId}", TodoShow)
   log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+  todos := Todos {
+    Todo { Name: "Write presentation" },
+    Todo { Name: "Host meetup" },
+  }
+  json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  todoId := vars["todoId"]
+  fmt.Fprintln(w, "Todo show:", todoId)
 }
