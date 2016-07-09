dbc561ca1aaee8c9eab7da351bc41f01e77083bd
Manuel Sanchez <manuelsanchez1100@gmail.com>
2016-07-08 23:17:29 -0500

    Updated repo to load json file

--- repo.go
+++ repo.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "log"
+)
+
 var currentId int
 var todos Todos
 
+type holiday []map[string]string
+
 func init() {
-    RepoCreateTodo(Todo { Name: "something" })
-    RepoCreateTodo(Todo { Name: "something else"})
+  var data holiday
+  file, err := ioutil.ReadFile("holidays.json")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  err = json.Unmarshal(file, &data)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  fmt.Println(data)
+
+  RepoCreateTodo(Todo { Name: "something" })
+  RepoCreateTodo(Todo { Name: "something else"})
 }
 
 func RepoFindTodo(id int) Todo {
